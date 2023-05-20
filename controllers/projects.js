ab27b95be1100048c701ba798ead39e4234ce603
k-vernon <kvernon908@gmail.com>
2023-05-20 02:08:31 -0400

    show project function

--- controllers/projects.js
+++ controllers/projects.js
@@ -22,10 +22,28 @@ const getAllProjects = async (req, res) => {
   }
 };
 
+const getProjectById = async (req, res) => {
+  try {
+    const project = await Project.findByPk(req.params.id);
+
+    if (!project) {
+      return res.status(404).json({ error: 'Project not found' });
+    }
+
+    res.status(200).json({ project });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to fetch project' });
+  }
+};
+
+
+
 
 module.exports = {
   createProject,
   getAllProjects,
-  
+  getProjectById,
+
 
 }
