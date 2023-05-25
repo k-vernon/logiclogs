a55d79f4e297a852d8ac10ab094c3be7ad63bcc4
k-vernon <kvernon908@gmail.com>
2023-05-24 23:09:54 -0400

    add project routes

--- routes/project.js
+++ routes/project.js
@@ -1,12 +1,29 @@
 const router = require('express').Router()
-const ticketsCtrl = require('../controllers/projects.js')
+const projectsCtrl = require('../controllers/projects.js')
 const middleware = require('../middleware/auth.js')
 
 const { decodeUserFromToken, checkAuth } = middleware
 
 /*---------- Public Routes ----------*/
 
+// Create 
+router.post('/', projectController.createProject);
+
+// Index
+router.get('/', projectController.getAllProjects);
+
+// Show 
+router.get('/:id', projectController.getProjectById);
+
+// Update
+router.put('/:id', projectController.updateProject);
+
+// Delete
+router.delete('/:id', projectController.deleteProject);
+
 
 /*---------- Protected Routes ----------*/
 
+
+module.exports = router
 
