c1bd6b9bca1cf8f248ce69b0497b7a12c6d4c7d9
k-vernon <kvernon908@gmail.com>
2023-06-28 23:16:32 -0400

    create ticket routes

--- routes/ticket.js
+++ routes/ticket.js
@@ -6,6 +6,21 @@ const { decodeUserFromToken, checkAuth } = middleware
 
 /*---------- Public Routes ----------*/
 
+// Create 
+router.post('/', ticketsCtrl.createTicket);
+
+// Index
+router.get('/', ticketsCtrl.getAllTickets);
+
+// Show 
+router.get('/:id', ticketsCtrl.getTicketById);
+
+// Update
+router.put('/:id', ticketsCtrl.updateTicket);
+
+// Delete
+router.delete('/:id', ticketsCtrl.deleteTicket);
+
 
 /*---------- Protected Routes ----------*/
 
